ec5ff8517e7eed218ed69b018a99575ffa792a68
Sergio k <valodya.kirik@gmail.com>
2022-10-27 16:18:18 +0300

    added my project

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,4 +1,29 @@
-// Add the required types and props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import cn from 'classnames';
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+import { Todo } from '../../types/Todo';
+import './TodoInfo.scss';
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoInfo: React.FC<Props> = (props) => {
+  const {
+    title,
+    user,
+    completed,
+  } = props.todo;
+
+  return (
+    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
+      <input type="checkbox" checked={completed} />
+
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,19 @@
-// Add the required types and props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import { FC } from 'react';
+import { Todo } from '../../types/Todo';
+import { TodoInfo } from '../TodoInfo';
+
+interface Props {
+  todoList: Todo[];
+}
+
+export const TodoList: FC<Props> = (props) => {
+  const { todoList } = props;
+
+  return (
+    <section className="TodoList">
+      {todoList.map((todo) => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
